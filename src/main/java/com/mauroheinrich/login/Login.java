acc9bbbacbd1c41160a44347aaf7aaf6e0f1dd31
Mauro Jesus Heinrich <heinrichmauro@gmail.com>
2024-05-02 15:08:23 -0300

    estructura del proyecto, diseño interface login

---
+++ src/main/java/com/mauroheinrich/login/Login.java
@@ -0,0 +1,11 @@
+
+
+package com.mauroheinrich.login;
+
+
+public class Login {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/mauroheinrich/login/logica/Usuario.java
@@ -0,0 +1,46 @@
+
+package com.mauroheinrich.login.logica;
+
+
+public class Usuario {
+    
+    private int id;
+    private String nombreUsuario;
+    private String contrasenia;
+
+    public Usuario() {
+    }
+
+    public Usuario(int id, String nombreUsuario, String contrasenia) {
+        this.id = id;
+        this.nombreUsuario = nombreUsuario;
+        this.contrasenia = contrasenia;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+    
+    
+    
+}
